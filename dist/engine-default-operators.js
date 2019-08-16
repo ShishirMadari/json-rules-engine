247a6b84f6ead6e8577db1990a4747377f68207e
Chase <chase@truepill.com>
2019-08-15 18:18:15 -0700

    build pkg

--- dist/engine-default-operators.js
+++ dist/engine-default-operators.js
@@ -32,10 +32,29 @@ Operators.push(new _operator2.default('doesNotContain', function (a, b) {
 }, Array.isArray));
 
 var containsOnly = function containsOnly(a, b) {
-  console.log(a, b);
-  for (var element in a) {
-    console.log("Test element", element);
-    if (b.indexOf(element) === -1) return false;
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = a[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var element = _step.value;
+
+      if (b.indexOf(element) === -1) return false;
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
   }
 
   return true;
